cc3c6082b49e608ed01409e2d959a0b0efbcbd12
Eusebiu Marcu <marcueusebiu@gmail.com>
2013-11-17 11:03:54 +0200

    add WP8 project

---
+++ RoboBeast.WP8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using RoboBeast.WP8.Resources;
+
+namespace RoboBeast.WP8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
