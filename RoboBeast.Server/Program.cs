e44789a9818688a32a2dea1020983e966e3bca28
Dodo <licadodo@gmail.com>
2013-11-17 11:14:51 +0200

    add client reference

--- RoboBeast.Server/Program.cs
+++ RoboBeast.Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Configuration;
 using System.ServiceModel;
+using System.ServiceModel.Description;
 
 namespace RoboBeast.Server
 {
@@ -8,8 +9,25 @@ namespace RoboBeast.Server
     {
         static void Main(string[] args)
         {
+
             using (ServiceHost host = new ServiceHost(typeof(RoboService)))
             {
+                // Check to see if the service host already has a ServiceMetadataBehavior
+                ServiceMetadataBehavior smb = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
+                // If not, add one
+                if (smb == null)
+                    smb = new ServiceMetadataBehavior();
+
+                smb.HttpGetEnabled = true;
+                smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                host.Description.Behaviors.Add(smb);
+
+                host.AddServiceEndpoint(
+                  ServiceMetadataBehavior.MexContractName,
+                  MetadataExchangeBindings.CreateMexHttpBinding(),
+                  "mex"
+                );
+
                 host.Open();
 
                 Console.WriteLine("Press <Enter> to stop the service");
